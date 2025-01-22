0293ca7f888b2d3de8f2381eebc5f7221e43d755
BaluPasumarthi <balupasumarthi1@gmail.com>
2025-01-22 18:20:44 +0530

    changes done

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { connectDb } = require("./config/database");
+const  cookieParser = require("cookie-parser");
+const cors = require('cors');
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials:true
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
+const userRouter = require("./routes/users");
+
+
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
+
+
+
+
+connectDb() 
+    .then(() =>{
+        console.log("Database Connected");
+        app.listen(1234, () => {
+            console.log("Server started on port 1234.....");
+        });
+    })
+    .catch((err) => {
+        console.log("Error:", err.message);
+    });
+
+    
