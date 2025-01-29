77e4ef425c0e35314e17c94301def3a0954b925c
PASUMARTHI BALU <131604638+Balu2200@users.noreply.github.com>
2025-01-29 19:01:38 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ const  cookieParser = require("cookie-parser");
 const cors = require('cors');
 
 const app = express();
-const corsOptions = {
-    origin: 'https://dev-connect-pi.vercel.app',
-    credentials: true, 
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials:true
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
