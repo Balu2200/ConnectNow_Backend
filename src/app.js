9274a090e76a4a51230d4aa6d4cdb2cba8ee3e8b
BaluPasumarthi <balupasumarthi1@gmail.com>
2025-02-15 11:45:25 +0530

    adde socket confirguration for chat

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const { connectDb } = require("./config/database");
 const  cookieParser = require("cookie-parser");
 const cors = require('cors');
 require("dotenv").config();
+const http = require("http");
+
+
 
 const app = express();
 app.use(
@@ -19,6 +22,7 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/users");
+const initilizeSocket = require("./utils/socket");
 
 
 app.use("/", authRouter);
@@ -26,13 +30,13 @@ app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 
-
-
+const server = http.createServer(app);
+initilizeSocket(server);
 
 connectDb() 
     .then(() =>{
         console.log("Database Connected");
-        app.listen(process.env.PORT, () => {
+        server.listen(process.env.PORT, () => {
             console.log("Server started on port 1234.....");
         });
     })
