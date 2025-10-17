f95ef30ea59c046783c56b9506f12944e961dacf
Balu <balupasumarthi1@gmail.com>
2025-10-17 22:37:25 +0530

    bug fixed: socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,15 +1,22 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
 
-const getSecretRoomId = (userId, targetUserId) =>{
-  return crypto.createHash("sha256").update([userId, targetUserId].sort().join("_")).digest("hex");
-}
-
+const getSecretRoomId = (userId, targetUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("_"))
+    .digest("hex");
+};
 
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: process.env.CLIENT_URL || "http://localhost:5173",
+      origin: [
+        process.env.CLIENT_URL || "http://localhost:5173",
+        "https://connect-now-frontend.vercel.app",
+      ],
+      credentials: true,
+      methods: ["GET", "POST"],
     },
   });
 
@@ -17,13 +24,13 @@ const initializeSocket = (server) => {
     console.log(`User connected: ${socket.id}`);
 
     socket.on("joinChat", ({ userId, targetUserId }) => {
-      const roomId = getSecretRoomId(userId,targetUserId);
+      const roomId = getSecretRoomId(userId, targetUserId);
       socket.join(roomId);
       console.log(`User ${userId} joined room: ${roomId}`);
     });
 
     socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
-      const roomId = getSecretRoomId(userId,targetUserId);
+      const roomId = getSecretRoomId(userId, targetUserId);
       io.to(roomId).emit("messageReceived", { firstName, text });
       console.log(`Message sent in ${roomId}: ${text}`);
     });
